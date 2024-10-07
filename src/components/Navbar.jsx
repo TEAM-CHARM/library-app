726f6fbd2b2986d6d4bb7057cfc74ddf966bc1af
Governor Narh <ron468896@gmail.com>
2024-10-07 15:41:21 +0000

    added test link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return <div>Navbar
+
+    <Link to='/dashboard'>Dashboard</Link>
+  </div>;
 };
 
 export default Navbar;
