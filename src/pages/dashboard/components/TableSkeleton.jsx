5b9dadfa81a6c94fe7dc8d0c05d9bc96718acfcc
Governor Narh <ron468896@gmail.com>
2024-10-08 14:51:46 +0000

    Start books integration

--- src/pages/dashboard/components/TableSkeleton.jsx
+++ src/pages/dashboard/components/TableSkeleton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../dashboard.css";
 
-const rows = [1, 2, 3, 4, 5,6 ];
+const rows = [1, 2, 3, 4, 5, ];
 const TableSkeleton = () => {
   return (
     <table className="min-w-full bg-white border-2 border-primary-light rounded-xl shadow-lg overflow-hidden">
@@ -16,9 +16,9 @@ const TableSkeleton = () => {
         </tr>
       </thead>
       <tbody>
-        {/* {rows.map((row, index) => {
-          return ( */}
-            <tr  className="animate-pulse">
+        {rows?.map((row, index) => {
+          return (
+            <tr key={index} className="animate-pulse">
               <td className="border px-4 py-2">
                 <div className="h-4 bg-gray-300 rounded"></div>
               </td>
@@ -39,50 +39,8 @@ const TableSkeleton = () => {
                 <div className="w-5 h-5 bg-gray-300 rounded"></div>
               </td>
             </tr>
-            <tr  className="animate-pulse">
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2 flex space-x-2 justify-center">
-                <div className="w-5 h-5 bg-gray-300 rounded"></div>
-                <div className="w-5 h-5 bg-gray-300 rounded"></div>
-              </td>
-            </tr>
-            <tr className="animate-pulse">
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2">
-                <div className="h-4 bg-gray-300 rounded"></div>
-              </td>
-              <td className="border px-4 py-2 flex space-x-2 justify-center">
-                <div className="w-5 h-5 bg-gray-300 rounded"></div>
-                <div className="w-5 h-5 bg-gray-300 rounded"></div>
-              </td>
-            </tr>
-          {/* );
-        })} */}
+          );
+        })}
       </tbody>
     </table>
   );
